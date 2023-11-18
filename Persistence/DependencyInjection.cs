38c45ba319e1245bae01862b18e8c453c51b9c15
ViktoriTanos <99451694+Viktoriaset@users.noreply.github.com>
2023-11-18 10:09:49 +1000

    Add MediatR for di

---
+++ Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Application.Interfaces;
+
+namespace Persistence
+{
+    internal static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration) 
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<UserDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<IUserDbContext>(provider =>
+                provider.GetService<UserDbContext>());
+            return services;
+        }
+    }
+}
